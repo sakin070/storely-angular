044cea185136cf87105a22fdfad452d7690cd9de
saheed <sakin070@uottawa.ca>
2021-04-26 19:22:16 -0400

    Nav bar selects home component on load

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  // @ts-ignore
+  @Input() itemId: number;
   constructor() { }
 
   ngOnInit(): void {
+    const item: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName('menu-item') as HTMLCollectionOf<HTMLElement> ;
+    item[this.itemId].style.filter =  'invert(53%) sepia(76%) saturate(3665%) hue-rotate(199deg) brightness(99%) contrast(97%)';
   }
 
   showSubMenu(event: Event): void{
@@ -21,7 +24,5 @@ export class NavBarComponent implements OnInit {
     const target = event.target as Element;
     // @ts-ignore
     target.parentElement.style.display = 'none';
-
   }
-
 }
