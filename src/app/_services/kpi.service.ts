7b717e20e923e70f49220c0225e673cb66c2519e
saheed <sakin070@uottawa.ca>
2021-07-14 21:20:47 -0400

    Complete Store Evaluation

--- src/app/_services/kpi.service.ts
+++ src/app/_services/kpi.service.ts
@@ -3,8 +3,8 @@ import {TokenStorageService} from './token-storage.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
-// const API = 'https://storley.herokuapp.com/analytics';
-const API = 'http://localhost:8080/analytics';
+const API = 'https://storley.herokuapp.com/analytics';
+// const API = 'http://localhost:8080/analytics';
 
 @Injectable({
   providedIn: 'root'
